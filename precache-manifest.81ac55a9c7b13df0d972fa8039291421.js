4886451a4a7a7e6372b2750b4014bad295dfa239
talha7t <48598366+talha7t@users.noreply.github.com>
2020-10-14 11:08:21 +0500

    Updates

---
+++ precache-manifest.81ac55a9c7b13df0d972fa8039291421.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24d574ee0bcc809824c351fba25534ce",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ca35e612892c3da16c9e",
+    "url": "/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "1f9344d933d80cca34ba",
+    "url": "/static/css/main.eb389a4f.chunk.css"
+  },
+  {
+    "revision": "ca35e612892c3da16c9e",
+    "url": "/static/js/2.bb90d2e6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.bb90d2e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f9344d933d80cca34ba",
+    "url": "/static/js/main.ab9ca135.chunk.js"
+  },
+  {
+    "revision": "101521af1dcb473a5c0e",
+    "url": "/static/js/runtime-main.2bcaa0ad.js"
+  },
+  {
+    "revision": "cbe2d9bf16848c67e859bce90d99fad6",
+    "url": "/static/media/about-construction.cbe2d9bf.jpg"
+  },
+  {
+    "revision": "40b45987534f19f04a0515999d765482",
+    "url": "/static/media/bg-mobile-menu.40b45987.jpg"
+  },
+  {
+    "revision": "ba06158402680d6709eec3ba1331f353",
+    "url": "/static/media/nav-bg.ba061584.jpg"
+  }
+]);
